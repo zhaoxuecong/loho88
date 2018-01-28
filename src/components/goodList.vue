2b6e7802401153d2d9d64468dcf47e03e8193fc4
wuhen <925886343@qq.com>
2018-01-28 22:49:46 +0800

    ceshi

--- src/components/goodList.vue
+++ src/components/goodList.vue
@@ -61,10 +61,6 @@ export default{
 		  this.loading = true;
 		  setTimeout(() => {
 		    
-//			let last = this.list[this.list.length - 1];
-//		    for (let i = 1; i <= 10; i++) {
-//		      this.list.push(last + i);
-//		    }
 			this.getData();
 		    this.loading = false;
 		  }, 2500);
